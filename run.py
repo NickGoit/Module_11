bb17f22b00969b7b3dc2600d80f261dbda5a5ad9
NickGoit <113237474+NickGoit@users.noreply.github.com>
2022-11-14 19:11:37 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,16 @@ from datetime import datetime
 
 class Field:
     def __init__(self, value):
+        self._value = None
         self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        self._value = new_value
 
 
 class Name(Field):
